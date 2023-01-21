22f7e9032dfc9a3eef8f195f28eecdc03de8d21c
gabrielpgf <gabrielpgf2011@gmail.com>
2023-01-20 22:12:24 -0300

    Conclusão do TODO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarphone Nokia:");
+Smartphone nokia = new Nokia(numero: "7785485631", modelo: "Lumia 750", imei: "dgs58798/waf480", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smarphone Iphone:");
+Smartphone iphone = new Iphone(numero: "2157844772", modelo: "Iphone 13 Pro Max", imei: "s874fry545safrv18%&%", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
